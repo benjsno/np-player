af5482e3c1b352684af64f16c8686bfb8a6f9e2d
Benjamin Chéré <benchere@yahoo.fr>
2015-02-23 15:59:11 +0100

    réorganisation dossiers

---
+++ README.md
@@ -0,0 +1,55 @@
+**Version**: v1.0
+
+*Déposez le dossier `np-player` à la racine du site. Les dossiers `css`, `pics` et le fichier `index.php` sont nécessaires uniquement pour l'exemple d'intégration.*
+
+NOUS PROD Player
+==========================
+Ce player HTML5 charge des vidéos depuis [Vimeo](https://vimeo.com/).
+Il propose, sous le player vimeo, des infos cliquables concernant la vidéo en lecture  et un menu de visuels cliquables lançant chacun une vidéo. 
+Les données du player sont chargées depuis un fichier JSON externe se trouvant dans le dossier np-player/data/
+
+## Usage ##
+
+###Définir les données du player###
+Créer un dossier dans np-player/data/ dans lequel on crée un fichier json du même nom.
+
+Par exemple 
+`np-player/data/2015-03-01/2015-03-01.json`
+ou
+`np-player/data/mars2015/mars2015.json`
+
+
+
+Données du fichier JSON :
+
+Pour chaque vidéo on doit définir
+
+-   title : le titre (ex: nom d'artiste)
+-   subtitle : le sous-titre (ex: date et lieu du concert)
+-   pict_url : l'URL de l'image qui s'affichera dans le menu
+-   vi_video_id : L'id de la vidéo Vimeo
+-   title_url : le lien quand on clique sur le titre (ex:fiche artiste)
+-   subtitle_url : le lien quand on clique sur le sous-titre (ex:fiche artiste, agenda concert ou réservation)
+-   book_url : le lien quand on clique sur "réserver" (ex:URL FNAC spectacle)
+
+```json
+{
+    "0":{
+		"title" : "The Prodigy",
+		"subtitle" : "15 avril 2015 - Le Zénith / Paris",
+		"pict_url" : "http://www.nousproductions.com/pics/data/artistes/photoBios/413-230x230.jpg",
+		"vi_video_id" : "97933438",
+		"title_url" : "http://www.nousproductions.com/artistes-the-prodigy,363.php",
+		"subtitle_url" : "http://www.nousproductions.com/concerts.php",
+		"book_url" : "http://www.fnacspectacles.com/place-spectacle/manifestation/Musique-electronique-THE-PRODIGY-ZPPRO.htm#/disponibilite/2fcf7dd8c0a8280b415df6c424781265/normale"
+	}
+}
+```
+
+
+###Intégrer le player dans une page###
+Le player avec menu est une iframe intégrable facilement. On indique à l'iframe quels données charger en passant le nom du dossier via une variable d (?d=nomdudossier)
+```html
+<iframe id="np-player" scrolling="no" frameborder="0" src="./np-player/iframe/np-player.php?d=2015-03-01" width="592" height="506"></iframe>
+```
+
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="Content-Style-Type" content="text/css">
+		<meta http-equiv="Content-Script-Type" content="text/javascript">
+		<meta http-equiv="Content-Language" content="fr">
+		<title>Nous Productions - Concerts, spectacles, événementiel</title>
+		<meta name="description" content="NOUS PRODUCTIONS - Organisation de concerts, spectacles, événementiel. Trouvez et achetez vos places pour les concerts des plus grands artistes internationaux et français.">
+		<meta name="keywords" content="nousproductions, nous productions, nous, productions, concerts, tournées, dates, organisation.">
+		<meta name="author" content="Nous Productions">
+		<meta name="copyright" content="www.nousproductions.com">
+		<meta name="robots" content="index, follow">
+		<meta property="og:title" content="Nous Productions - Concerts, spectacles, événementiel">
+		<meta property="og:url" content="http://www.nousproductions.com/">
+		<meta property="og:image" content="http://www.nousproductions.com/pics/charte/bg-logo.png">
+		<meta property="og:site_name" content="Nous Productions">
+		<link type="text/css" rel="stylesheet" href="css/nous.css" media="screen">
+	</head>
+	
+	<body>
+		<div id="wrapper">
+			<div id="global">
+			  	<div id="header">
+					<a id="logo" href="index.php" title="Nous Productions - Accueil">Nous Productions - Accueil</a>
+					<div id="banniere"><div style="background:#DDD;width:730px;height:90px"></div></div>
+				</div>
+				
+			    <div id="article">
+			    
+			    	<iframe id="np-player" scrolling="no" frameborder="0" src="./np-player/iframe/np-player.php?d=2015-01-06" width="592" height="506"></iframe>
+			    
+			    	<div style="background:#DDD;height:2000px"></div>  	
+			    
+			    </div>
+				
+			      
+			    <div id="colonne">
+			    	<div style="background:#DDD;height:1200px"></div>  	
+				</div>
+				<div class="clear"></div>
+				
+				<div id="footerContainer">
+					<div id="footer">
+						<p id="footerRss"><a href="news-feed.php" title="FLUX RSS Nous Productions">FLUX RSS</a></p>
+						<p id="footerMain">
+							<a href="artistes.php" title="Tous les artistes">ARTISTES</a> -
+							<a href="concerts.php" title="Tous les concerts">CONCERTS</a> -
+							<a href="news.php" title="Toutes les news">NEWS</a> -
+							<a href="live-report.php" title="Tous les live reports">LIVE REPORT</a> -
+							<a href="concours.php" title="Tous les concours">CONCOURS</a> -
+							<a href="newsletter.php" title="Inscrivez-vous à la newsletter Nous productions">NEWSLETTER</a> -
+							<a href="pro.php" title="">PRO</a> -
+							<a href="contact.php" title="Contacter Nous Productions">CONTACT</a>
+						</p>
+						<p id="footerSec">
+							© 2015 - Nous Productions - All rights reserved - 118, rue du Mont Cenis - 75018 Paris - Tél : +33 (0)1 48 24 12 20 -
+							<a href="mentions-legales.php">Mentions légales</a> -
+							<a href="http://danstouslessens.com" target="_blank" title="Design graphique">Conception</a> –
+							<a href="http://www.e-lixir.fr" target="_blank" title="Réalisation sites internet">Développement </a>
+						</p>
+					</div>
+				</div>
+			</div> <!-- end global -->	
+		</div> <!-- end wrapper -->	
+	</body>
+</html>
